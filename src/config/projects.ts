c1b988461db9cb0bd9a54a2c5765f4b3ce7e271e
Mxcoc <37839658+Mxcoc@users.noreply.github.com>
2025-06-28 22:21:30 +0800

    更新 projects.ts

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -19,54 +19,6 @@ export type ProjectItemType = {
   // projects 
   export const projects: Array<ProjectItemType> = [
     {
-      name: '轻息',
-      description:
-        '一款呼吸训练、缓解压力、提升专注力的“小而美”app',
-      link: { href: 'apps.apple.com/us/app/%E8%BD%BB%E6%81%AF/id6742171445', label: '轻息' },
-      category: ['App'],
-      techStack: ['SwiftUI'],
-      tags: ['App', 'iOS'],
-      logo: '/images/icon/breathe-app-icon.png',
-    },
-    {
-      name: 'SEO Explore',
-      description:
-        'Explore all the best SEO tools in one place.',
-      link: { href: 'seoexplore.com', label: 'SEO Explore' },
-      category: ['Website'],
-      techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
-      tags: ['SEO']
-    },
-    {
-      name: 'GitHub Cards',
-      description:
-        'Showcase your GitHub contributions into stunning visual cards.',
-      link: { href: 'github.cards', label: 'GitHub Cards' },
-      category: ['Website'],
-      techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
-      tags: ['Visual Cards', 'GitHub Contribution Cards']
-    },
-    {
-      name: 'AI Best Tools',
-      description:
-        'Find the best AI tools in AIBest.tools',
-      link: { href: 'aibest.tools', label: 'AI Best Tools' },
-      logo: 'images/icon/aibesttools.png',
-      category: ['Website'],
-      techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
-      tags: ['AI', 'Tools Directory']
-    },
-    {
-      name: 'Best Directories',
-      description:
-        'Your ultimate directory of directories.',
-      link: { href: 'bestdirectories.org', label: 'Best Directories' },
-      logo: '/images/icon/bestdirectories.png',
-      category: ['Website'],
-      techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
-      tags: ['Directory of Directories']
-    },
-    {
       name: 'User Growth',
       description:
         'Boost Your business growth with UserGrowth.link',

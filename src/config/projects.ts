65c74d30c9711c50ccf08c202d00cc4b568a0a46
Mxcoc <37839658+Mxcoc@users.noreply.github.com>
2025-06-28 22:29:16 +0800

    更新 projects.ts

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -27,37 +27,7 @@ export type ProjectItemType = {
       category: ['Website'],
       techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
       tags: ['User Growth', 'Marketing', 'SEO']
-    },
-    {
-      name: 'Dev Toolset',
-      description:
-        'Open-source database-free tools directory.',
-      link: { href: 'devtoolset.net', label: 'Dev Toolset' },
-      logo: '/images/icon/devtoolset.png',
-      category: ['Website'],
-      techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
-      tags: ['Open Source', 'Database-Free', 'Tools Directory']
-    },
-    {
-      name: 'Domain Score',
-      description:
-        'Ultimate AI-Powered tool for domain scoring and evaluation',
-      link: { href: 'domainscore.ai', label: 'Domain Score' },
-      logo: '/images/icon/domainscore.png',
-      category: ['Website'],
-      techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
-      tags: ['Domain', 'AI', 'SEO']
-    },
-    {
-      name: 'MagicBox Tools',
-      description:
-        'Find the best AI tools in MagicBox.tools',
-      link: { href: 'magicbox.tools', label: 'MagicBox Tools' },
-      logo: '/images/icon/magicbox.png',
-      category: ['Website'],
-      techStack: ['Next.js', 'TailwindCSS', 'Shadcn/UI'],
-      tags: ['AI', 'Tools Directory']
-    },
+    }
   ]
   
   export const githubProjects: Array<ProjectItemType> = [
@@ -67,46 +37,6 @@ export type ProjectItemType = {
       link: { href: 'github.com/iAmCorey/devtoolset', label: 'Devtoolset' },
       gitStars: 203,
       gitForks: 67
-    },
-    {
-      name: 'Corey Chiu Portfolio Template',
-      description:
-        'portfolio template by corey chiu',
-      link: { href: 'github.com/iAmCorey/coreychiu-portfolio-template', label: 'Corey Chiu Portfolio Template' },
-      gitStars: 229,
-      gitForks: 30
-    },
-    {
-      name: 'Chrome Extension Plasmo Template',
-      description:
-        'A chrome extension template using plasmo, tailwind css, shadcn/ui',
-      link: { href: 'github.com/iAmCorey/chrome-extension-plasmo-template', label: 'Chrome Extension Plasmo Template' },
-      gitStars: 54,
-      gitForks: 10
-    },
-    {
-      name: 'Awesome Indie Hacker Tools',
-      description:
-        '独立开发/出海开发相关技术栈及工具收录 / Find the best tools for indie hackers here',
-      link: { href: 'github.com/iAmCorey/awesome-indie-hacker-tools', label: 'Awesome Indie Hacker Tools' },
-      gitStars: 815,
-      gitForks: 69
-    },
-    {
-      name: 'Awesome AI Directory',
-      description:
-        'AI资源工具导航站收录 / Find all the best AI directories',
-      link: { href: 'github.com/iAmCorey/awesome-ai-directory', label: 'Awesome AI Directory' },
-      gitStars: 40,
-      gitForks: 7
-    },
-    {
-      name: 'Producthunt Daily Bot',
-      description:
-        'A bot getting product hunt daily top products',
-      link: { href: 'github.com/iAmCorey/producthunt-daily-bot', label: 'Producthunt Daily Bot' },
-      gitStars: 3,
-      gitForks: 3
     },
     {
       name: 'Cantonese Echoes',

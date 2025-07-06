c6e73db26d6ccb76fccd5a65e0ea722f1836395b
Mxcoc <37839658+Mxcoc@users.noreply.github.com>
2025-07-06 17:52:07 +0800

    更新 activity.ts

--- src/config/activity.ts
+++ src/config/activity.ts
@@ -1,6 +1,8 @@
 // activity
+{/*
 export const activityHeadLine = "My Activity"
 export const activityIntro = "Snippets of My Coding & Life"
+*/}
 
 export const tweetIds = [
   '1871125849019424839',

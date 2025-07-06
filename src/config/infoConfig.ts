677d64cd2b80ca06324b7d8592f621dc5d26f202
Mxcoc <37839658+Mxcoc@users.noreply.github.com>
2025-07-06 17:36:44 +0800

    更新 infoConfig.ts

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -13,15 +13,15 @@ export const introduction = 'Cattk | 业余无线电 HAM | 短波通信玩家 | 
 export const email = 'cattk@cattk.com'
 export const githubUsername = 'Mxcoc'
 
-/*
+
 // about page
-export const aboutMeHeadline = "I'm Corey Chiu, a software engineer based in Shenzhen, China."
-export const aboutParagraphs = [
-  "I love coding. I learned programming when I in college. I wrote my first program in Java when I was 18.",
-  "I have a lot of hobbies, such as travelling, photography, watching movies, music and so on.",
-  "I'm working as a software develop engineer in Shenzhen, China now. And I'm building a lot of side projects in my spare time."
-]
-*/
+// export const aboutMeHeadline = "I'm Corey Chiu, a software engineer based in Shenzhen, China."
+// export const aboutParagraphs = [
+//   "I love coding. I learned programming when I in college. I wrote my first program in Java when I was 18.",
+//   "I have a lot of hobbies, such as travelling, photography, watching movies, music and so on.",
+//   "I'm working as a software develop engineer in Shenzhen, China now. And I'm building a lot of side projects in my spare time."
+// ]
+
 
 
 // blog

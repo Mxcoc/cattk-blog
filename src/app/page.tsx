591c1a95073b8fe126ccf71a1020e292525f72dc
Mxcoc <37839658+Mxcoc@users.noreply.github.com>
2025-07-11 00:00:22 +0800

    更新 page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,6 +48,7 @@ export default async function Home() {
           <GitHubSnake />
         </div>
         {/* projects */}
+{/*
         <div className="mx-auto flex flex-col max-w-xl gap-6 lg:max-w-none my-4 py-8 border-t border-muted">
           <h2 className="text-3xl font-semibold tracking-tight md:text-5xl opacity-80">
             {projectHeadLine}
@@ -69,7 +70,6 @@ export default async function Home() {
             <CustomIcon name='github' size={28}/>
             Open Source
           </h2>
-{/*
           <ul
             role="list"
             className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 md:grid-cols-3"
@@ -78,8 +78,8 @@ export default async function Home() {
               <GithubProjectCard key={project.name} project={project} titleAs='h3'/>
             ))}
           </ul>
-*/}
         </div>
+*/}
         <div className="mx-auto flex flex-col max-w-xl gap-6 py-8 my-8 lg:max-w-none border-t border-muted">
           <h2 className="text-3xl font-semibold tracking-tight md:text-5xl opacity-80">
             {blogHeadLine}

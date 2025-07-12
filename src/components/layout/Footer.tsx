924a07d789e5d8137709520366b7346227f425ae
Mxcoc <37839658+Mxcoc@users.noreply.github.com>
2025-07-12 23:41:03 +0800

    更新 Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -27,7 +27,7 @@ function NavLink({
 
 export function Footer() {
   return (
-    <footer className="mt-12 flex-none">
+    <footer className="mt-6 flex-none">
       <ContainerOuter>
         <div className="border-t border-muted pb-16 pt-10">
           <ContainerInner>

1ae8e6cfe671980d8b1fef000bb4c704a6e4dbb5
Mxcoc <37839658+Mxcoc@users.noreply.github.com>
2025-07-13 12:52:15 +0800

    创建 BlogCard.tsx

---
+++ src/components/blog/BlogCard.tsx
@@ -0,0 +1,56 @@
+// src/components/blog/BlogCard.tsx
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { formatDate } from '@/lib/formatDate'
+import { type BlogType } from '@/lib/blogs'
+
+export function BlogCard({ blog }: { blog: BlogType }) {
+  return (
+    <Link href={`/blogs/${blog.slug}`}>
+      <article className="group relative flex flex-col items-start">
+        {/* 图片部分 */}
+        {blog.coverImage && (
+          <div className="relative w-full aspect-video mb-6 overflow-hidden rounded-2xl border border-zinc-100 dark:border-zinc-800">
+            <Image
+              src={blog.coverImage}
+              alt={blog.title}
+              fill
+              className="object-cover object-center transition-all duration-300 group-hover:scale-105"
+            />
+          </div>
+        )}
+
+        {/* 文字部分 */}
+        <div className="flex w-full flex-col">
+          <time
+            className="relative z-10 order-first mb-3 flex items-center pl-3.5 text-sm text-zinc-400 dark:text-zinc-500"
+            dateTime={blog.date}
+          >
+            <span
+              className="absolute inset-y-0 left-0 flex items-center"
+              aria-hidden="true"
+            >
+              <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+            </span>
+            {formatDate(blog.date)}
+          </time>
+          <h2 className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+            {blog.title}
+          </h2>
+          {blog.description && (
+            <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+              {blog.description}
+            </p>
+          )}
+          <div
+            aria-hidden="true"
+            className="relative z-10 mt-4 flex items-center text-sm font-medium text-primary"
+          >
+            Read article
+          </div>
+        </div>
+      </article>
+    </Link>
+  )
+}

8e9ce32ac07b838cd14b6d940d188a0a762cf35a
Mxcoc <37839658+Mxcoc@users.noreply.github.com>
2025-08-03 02:24:38 +0800

    更新 infoConfig.ts

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -4,7 +4,7 @@ export * from './changelog'
 
 
 // personal info
-export const name = "Cattk's Blog"
+export const name = "Cattk Blog"
 export const headline = '吃枣药丸.'
 export const introduction = 'Cattk | 业余无线电 HAM | 短波通信玩家 | 电子 DIY 爱好者。用天线捕捉远方的故事，用焊烙铁筑造心中的奇思。'
 export const email = 'cattk@cattk.com'
